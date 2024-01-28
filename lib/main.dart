1c04c363d68681fbf27c43843ecf36a8daf69a73
KalinaBujko <kiki.b2002@gmail.com>
2024-01-28 21:52:20 +0100

    Gra Flappy Bird
    Popularny Flappy Bird stworzony w 100% w flutterze.

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flappybird/ekrany/game_over.dart';
+import 'package:flappybird/ekrany/main_menu.dart';
+import 'package:flappybird/game/flappybird_game.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future<void> main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  WidgetsFlutterBinding.ensureInitialized();
+  await Flame.device.fullScreen();
+
+  final game = FlappyBirdGame();
+  runApp(
+    GameWidget(
+      game: game,
+      initialActiveOverlays: const [MainMenuScreen.id],
+      overlayBuilderMap: {
+        'mainMenu': (context, _) => MainMenuScreen(game: game),
+        'gameOver': (context, _) => GameOverScreen(game: game),
+      },
+    ),
+  );
+}
